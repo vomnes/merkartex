09980dbd65b0a34dbf099c9d938bef4fbb5ef3c5
Valentin Omnès <valentin.omnes@gmail.com>
2020-05-27 18:50:28 +0200

    feat: Read file route

--- maps/src/routes/upload.ts
+++ maps/src/routes/upload.ts
@@ -1,14 +1,18 @@
 import { Router, Request, Response } from "express";
 import * as multer from 'multer';
+import * as fs from 'fs';
 
 const router = Router();
 
 const upload = multer({ dest: './public/data/uploads/' });
 
 router.post('/file', upload.single('map'), (req: Request, res: Response) => {
-  console.log('->', req);
-  console.log('=>', res);
-  res.send("_Upload_");
-})
+  if (!req.file) {
+    res.send('Empty file\n');
+    return
+  }
+  const data = fs.readFileSync(req.file.path, 'utf8');
+  res.send(data);
+});
 
 export { router as Upload };
