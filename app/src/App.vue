30b4dab5675234e57cd6ff62489e8bb039d45577
Valentin Omnès <valentin.omnes@gmail.com>
2020-06-02 15:24:41 +0200

    feat: Add base Vuejs app

---
+++ app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ManageKMZ msg="Hello"/>
+  </div>
+</template>
+
+<script>
+import ManageKMZ from './components/ManageKMZ/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ManageKMZ,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/ManageKMZ/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="manage-kmz">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
