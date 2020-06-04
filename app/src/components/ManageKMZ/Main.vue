d933885feb75ac85f44231bf4ddb596352d14b1a
Valentin Omnès <valentin.omnes@gmail.com>
2020-06-04 16:18:24 +0200

    feat: Update layout to grid css + side code placemark

--- app/src/components/ManageKMZ/Main.vue
+++ app/src/components/ManageKMZ/Main.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="manage-kmz">
-    <div class="map"></div>
+    <div class="header">
+      <div class="header--title">
+        <svg v-svg symbol="kmz"></svg>
+        <h1>Shanghai.kmz</h1>
+      </div>
+    </div>
+    <div class="map">
+      <!-- https://cloud.maptiler.com/maps/voyager -->
+    </div>
     <div class="placemarks">
       <h1 class="text__title">Placemarks</h1>
       <div class="placemarks__list custom-scrollbar">
@@ -34,8 +42,37 @@ export default {
   @import './Placemarks.scss';
 
   .manage-kmz {
-    display: flex;
+    display: grid;
+    grid-template-columns: 3fr 60rem;
+    grid-template-rows: 6.5rem auto;
 
-    margin: 2rem 2rem 0 2rem;
+    margin: 1rem 2rem 0 2rem;
+
+    & .header {
+      grid-column-start: 1;
+      grid-column-end: 2;
+
+      &--title {
+        display: flex;
+        align-items: center;
+
+        & h1 {
+          font-family: $font-3;
+          font-weight: 400;
+          font-size: 2.4rem;
+        }
+
+        & svg {
+          width: 3rem;
+          height: 4.2rem;
+          margin-right: 1.5rem;
+        }
+      }
+    }
+
+    & .map {
+      grid-column-start: 2;
+      grid-column-end: 1;
+    }
   }
 </style>
