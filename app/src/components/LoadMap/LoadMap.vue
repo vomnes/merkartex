af40d840d975c684a2fabb92ea2d2f10db5debf7
Valentin Omnès <valentin.omnes@gmail.com>
2020-06-25 18:18:28 +0200

    feat: Add logic load map

--- app/src/components/LoadMap/LoadMap.vue
+++ app/src/components/LoadMap/LoadMap.vue
@@ -5,56 +5,111 @@
         <h1>Welcome on Merkartex</h1>
         <h2 class="text__details text--uppercase">Load your map 🚀</h2>
       </div>
-      <div class="load-map__uploaded-file">
-        <svg v-svg symbol="kmz"></svg>
-        <div class="load-map__uploaded-file--content">
-          <div class="load-map__uploaded-file--content__up">
-            <h3 class="text__body">Shanghai.kmz</h3>
-            <svg v-svg symbol="close"></svg>
-          </div>
-          <div class="load-map__uploaded-file--content__bar">
-            <div
-              class="load-map__uploaded-file--content__bar--filled"
-              :style="{ width: `${95}%` }">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="load-map__upload-section">
+      <transition name="fade-upload"></transition>
+      <UploadedFile
+      v-if="selectedFile.valid"
+      :filename="selectedFile.name"
+      :loaded="selectedFile.loaded"
+      @clear="clearFile"/>
+      <div class="load-map__upload-section" v-if="!selectedFile.valid">
         <h3 class="text__body">1. MapsMe</h3>
-        <button class="primary-button--blue-white box-round-corner">
+        <button class="primary-button--blue-white box-round-corner"
+          @click="uploadMapsMeFile">
           <svg v-svg symbol="arrow"></svg>
           <p class="text__details text--uppercase">Upload MapsMe KMZ Map</p>
         </button>
+        <input @change="fileSelected" ref="mapsmeFileInput"
+        type="file" accept=".kmz">
       </div>
-      <div class="load-map__upload-section">
+      <div class="load-map__upload-section" v-if="!selectedFile.valid">
         <h3 class="text__body">2. Google Maps</h3>
         <button class="primary-button--blue-white box-round-corner">
           <svg v-svg symbol="arrow"></svg>
           <p class="text__details text--uppercase">Upload Google Maps KMZ Map</p>
         </button>
-        <button class="primary-button--blue-white box-round-corner">
+        <button class="primary-button--blue-white box-round-corner" @click="gmapsPage">
           <svg v-svg symbol="link"></svg>
           <p class="text__details text--uppercase">Upload map from Google Maps Page</p>
         </button>
       </div>
-      <div class="load-map__submit">
+      <div class="load-map__submit" v-if="selectedFile.valid">
         <button
           class="primary-button--green box-round-corner">
           <p class="text__body">Load your map</p>
         </button>
       </div>
+      <transition name="fade-error">
+        <p class="load-map__error text__body" v-if="error">{{ error }}</p>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
+import UploadedFile from './UploadedFile.vue';
+
 export default {
   name: 'LoadMap',
+  components: {
+    UploadedFile,
+  },
   data() {
     return {
-
+      selectedFile: {
+        name: '', loaded: 0, size: 0, valid: false,
+      },
+      error: null,
     };
+  },
+  methods: {
+    uploadMapsMeFile() {
+      this.$refs.mapsmeFileInput.click();
+    },
+    fileSelected(event) {
+      const file = event.target.files[0];
+      if (file && file.size > 100000) {
+        this.setError('File size limited to 100MB');
+        return;
+      }
+      if (file && !file.name.endsWith('.kmz')) {
+        this.setError('Support only KMZ files type');
+        return;
+      }
+      this.clearError();
+      this.selectedFile.name = file.name;
+      this.selectedFile.size = file.size;
+      this.selectedFile.valid = true;
+      const reader = new FileReader();
+      // let uploadedFile = null;
+      reader.onloadstart = this.readerEvent;
+      reader.onload = this.readerEvent;
+      reader.onprogress = this.readerEvent;
+      // uploadedFile = reader.result;
+      if (file) {
+        reader.readAsText(file);
+      }
+    },
+    readerEvent(e) {
+      this.selectedFile.loaded = Math.round((e.loaded * 100) / e.total);
+    },
+    clearFile() {
+      this.selectedFile = {
+        name: '', loaded: 0, size: 0, valid: false,
+      };
+    },
+    setError(error) {
+      this.error = error;
+    },
+    clearError() {
+      this.error = null;
+    },
+    gmapsPage() {
+      if (this.error) {
+        this.clearError();
+      } else {
+        this.setError('Not available');
+      }
+    },
   },
 };
 </script>
---
+++ app/src/components/LoadMap/UploadedFile.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="uploaded-file">
+    <svg v-svg symbol="kmz"></svg>
+    <div class="uploaded-file--content">
+      <div class="uploaded-file--content__up">
+        <h3 class="text__body" :title="filename">{{ filenameDisplay }}</h3>
+        <svg v-svg symbol="close" @click="$emit('clear')"></svg>
+      </div>
+      <progress :value="loaded" max="100" v-if="loaded < 100"></progress>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UploadedFile',
+  props: {
+    filename: String,
+    loaded: Number,
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    filenameDisplay() {
+      const lessExtension = this.filename.replace('.kmz', '');
+      return lessExtension.length < 17 ? this.filename : `${lessExtension.substring(0, 17)}... .kmz`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/_main.scss";
+
+.uploaded-file {
+  display: flex;
+  align-items: flex-end;
+  height: 5rem;
+
+  & > svg {
+    height: 100%;
+    max-width: 4.5rem;
+    margin-right: 1.4rem;
+  }
+
+  &--content {
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+
+    justify-content: flex-end;
+
+    &__up {
+      display: flex;
+      justify-content: space-between;
+      height: 2rem;
+
+      margin-bottom: .5rem;
+
+      & > h3 {
+        font-weight: 400;
+        max-width: 25rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      & > svg {
+        padding: .25rem;
+        width: 2rem;
+        border-radius: .15rem;
+        transition: all .3s;
+
+        &:hover {
+          background: $color-blue-2;
+          color: $color-primary-white;
+          cursor: pointer;
+          padding: .35rem;
+          transition: all .3s;
+        }
+      }
+    }
+
+    & progress {
+      height: .7rem;
+      width: 100%;
+
+      border: none;
+
+      &::-webkit-progress {
+        &-bar {
+          background: #A1C3E6;
+        }
+        &-value {
+          height: 100%;
+          background: linear-gradient(90deg, $color-blue 0%, #A1C3E6 200%);
+          transition-delay: .25s;
+          transition-duration: .5s;
+          transition-timing-function: cubic-bezier(0.22, 1, 0.36, 1);
+        }
+      }
+
+      &::-moz-progress {
+        &-bar {
+          height: 100%;
+          background: linear-gradient(90deg, $color-blue 0%, #A1C3E6 200%);
+        }
+      }
+    }
+
+    &__bar {
+      height: .7rem;
+      width: 100%;
+
+      background: #A1C3E6;
+
+      &--filled {
+        height: 100%;
+        background: linear-gradient(90deg, $color-blue 0%, #A1C3E6 200%);
+      }
+    }
+  }
+
+  margin: 2rem 0;
+}
+</style>
